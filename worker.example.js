8608ea4eb356dd4ff9ed8281a5b560102f2cda1d
awaradeveloper <adhikarinetramohan@gmail.com>
2026-09-04 18:10:15 +0530

    Add files via upload

--- worker.example.js
+++ worker.example.js
@@ -20,10 +20,10 @@ const LOG_PREFIX = 'LOG:';
 const ADMIN_KEY = 'ADMIN_USERS';
 
 const authConfig = {
-    "siteName": "SSBots", // Your Website name
-    "client_id": "576175951684-4qqn21it7ajov80emtn6at4oku99d80e.apps.googleusercontent.com", // Your Client id from Google Cloud Console
-    "client_secret": "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", // Your Client Secret from Google Cloud Console
-    "refresh_token": "1//0gg_8Ox43AAthCgYIARAAGBASNwF-L9Irb05nr0Xqc_q5jtvHPNG1onqMteLpYScfdK4yIYXjKyzgwBCQUdoUeK1qCC-Hf9KNaBc", // Your Authorize token - Get from OAuth Playground
+    "siteName": "SS Bots", // Your Website name
+    "client_id": "", // Your Client id from Google Cloud Console
+    "client_secret": "", // Your Client Secret from Google Cloud Console
+    "refresh_token": "", // Your Authorize token - Get from OAuth Playground
     "service_account": false, // true if you're using Service Account instead of user account
     "service_account_json": randomserviceaccount, // don't touch this one
     "files_list_page_size": 100,
@@ -62,8 +62,8 @@ const authConfig = {
     ],
     "roots": [
         {
-            "id": "1WLOY8hXo2oHBDs8tbT1-khjo3J8nwiYC", // Get from Google Drive folder URL
-            "name": "index",
+            "id": "8iugu89y09oioi6676", // Get from Google Drive folder URL
+            "name": "SSBot",
             "username": "", "password": "", // Optional folder-level auth
             "protect_file_link": false
         },
